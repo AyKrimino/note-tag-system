12aedbaf030b85528038d5473c314da1ea0af31b
AyKrimino <ayoub.krimi14@gmail.com>
2025-12-28 00:01:47 +0100

    feat(note-service): add service layer

--- services/note-service/cmd/note-service/main.go
+++ services/note-service/cmd/note-service/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/AyKrimino/note-tag-system/note-service/internal/domain"
 	"github.com/AyKrimino/note-tag-system/note-service/internal/logger"
 	"github.com/AyKrimino/note-tag-system/note-service/internal/repository"
+	"github.com/AyKrimino/note-tag-system/note-service/internal/service"
 )
 
 func main() {
@@ -36,9 +37,11 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// TODO: remove this after creating service
+	// TODO: remove this after creating handlers
 	noteRepo := repository.NewPostgresNoteRepository(pg.DB())
-	err = noteRepo.Create(ctx, &domain.Note{
+	noteSvc := service.NewNoteService(noteRepo, log)
+
+	err = noteSvc.Create(ctx, &domain.Note{
 		Title:   "Title",
 		Content: "Content",
 		Tags:    []string{"tag1", "tag2"},
@@ -47,9 +50,7 @@ func main() {
 		log.Error("failed to create note", slog.Any("error", err))
 	}
 
-	log.Info("created note")
-
-	n, err := noteRepo.GetByID(ctx, 1)
+	n, err := noteSvc.GetByID(ctx, 1)
 	if err != nil {
 		log.Error("failed to get note", slog.Any("error", err))
 	}
---
+++ services/note-service/internal/service/note_service.go
@@ -0,0 +1,39 @@
+// Package service contains the service for the notes
+package service
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/AyKrimino/note-tag-system/note-service/internal/domain"
+	"github.com/AyKrimino/note-tag-system/note-service/internal/repository"
+)
+
+// noteService is the service for the notes
+type noteService struct {
+	repo repository.NoteRepository
+
+	log *slog.Logger
+}
+
+// NewNoteService returns a new note service
+func NewNoteService(repo repository.NoteRepository, log *slog.Logger) *noteService {
+	return &noteService{
+		repo: repo,
+		log: log,
+	}
+}
+
+// GetByID returns a note by id
+func (s *noteService) GetByID(ctx context.Context, id int) (*domain.Note, error) {
+	s.log.Info("getting note by id", slog.Int("id", id))
+
+	return s.repo.GetByID(ctx, id)
+}
+
+// Create creates a note
+func (s *noteService) Create(ctx context.Context, note *domain.Note) error {
+	s.log.Info("creating note", slog.Any("note", note))
+
+	return s.repo.Create(ctx, note)
+}
